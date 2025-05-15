82282ecd34f5d507ae6a8e80bee267e216f4cf80
david <dclaydon@gmail.com>
2025-05-15 13:44:31 +0100

    added block markdown file

---
+++ src/block_markdown.py
@@ -0,0 +1,17 @@
+class BlockType(Enum):
+	PARAGRAPH = "Paragraph Text"
+	HEADING = "Heading Text"
+	CODE = "Code Text"
+	QUOTE = "Quote Text"
+	UNORDERED_LIST = "Unordered List"
+	ORDERED_LIST = "Ordered List"
+
+
+def markdown_to_blocks(markdown):
+	blocks = markdown.strip().split("\n\n")
+
+	for b in blocks:
+		if b == "":
+			blocks.remove(b)
+
+	return blocks

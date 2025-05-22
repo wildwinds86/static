2dcaed52ab8cf90ae5778081ffec6b0ec3108bb0
David <dclaydon@gmail.com>
2025-05-22 10:52:47 +0100

    finished recursive page generator

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,9 +1,8 @@
 import os
-import shutil
+#import shutil
 from inline_markdown import *
-from htmlnode import *
 from markdown_blocks import *
-from textnode import *
+
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page {from_path} to {dest_path} using {template_path}")
@@ -31,3 +30,21 @@ def generate_page(from_path, template_path, dest_path):
     f = open(dest_path, "x")
     f.write(html_page)
     f.close()
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+        print(f"Creating directory {dest_dir_path}")
+
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        #print(f" * {from_path} -> {dest_path}")
+
+        if os.path.isfile(from_path):
+            html_file_path = os.path.splitext(dest_path)[0] + ".html"
+            print(f"Generating html page from {from_path} to {html_file_path}")
+            generate_page(from_path, template_path, html_file_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,23 +1,24 @@
 import os
 import shutil
-from generate_page import generate_page
-
+from generate_page import generate_page, generate_pages_recursive
 from copystatic import copy_files_recursive
 
 
-dir_path_static = r"./static"
-dir_path_public = r"./public"
-dir_path_content = r"./content"
-file_path_template = r"./template.html"
+dir_path_static = r"../static"
+dir_path_public = r"../public"
+dir_path_content = r"../content"
+file_path_template = r"../template.html"
 
 
 def main():
-    print("Deleting public directory...")
     if os.path.exists(dir_path_public):
+        print("Deleting public directory...")
         shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     #generate_page(dir_path_content + "/index.md", file_path_template, dir_path_public + "/index.html")
+    generate_pages_recursive(dir_path_content, file_path_template, dir_path_public)
+
 main()
